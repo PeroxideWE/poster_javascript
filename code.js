59510b2ddc3840281ee69abcdaf346d3268d6a87
William Eriksson <william.eriksson85@gmail.com>
2018-03-13 17:12:37 +0100

    automagik

--- code.js
+++ code.js
@@ -1,5 +1,9 @@
+let $ = require("./node_modules/jquery");
+let movieData = require("./data");
 function renderMovie(movie){
     
+    
+    console.log("BUUUUUUUUUU??? jag?", movie)
     let title= document.getElementById("movieTitle");
     title.innerText=movie.movieTitle;
     
@@ -10,7 +14,7 @@ function renderMovie(movie){
     imageIndex.setAttribute("src", movie.moviePicture)
     
     // var text= document.getElementById("movieActors").innerHTML;
-
+    
     for (i = 0; i < movie.length; i++) { 
         var node = document.createElement("LI");
         var textnode = document.createTextNode(movie.movieActors);
@@ -18,25 +22,24 @@ function renderMovie(movie){
         document.getElementById("movieActors").appendChild(node); 
         //document.querySelector(".movieActors").appendChild(node);
     }
-
-    function changeStarRating(rating){
-        for(let i=1; i<=5; i++){
-          let star = document.getElementById("star" + i);
-          if (i <= rating){
-            star.classList.add("filled");
-          } else {
-            star.classList.remove("filled");
-          }
-        }
-      }
-      
-      for(let i=1; i<=5; i++){
-        let star = document.getElementById("star" + i);
-        star.addEventListener("click", function(){
-          changeStarRating(i);
-        });
-    }
-
-
 }; 
+
+function changeStarRating(rating){
+    for(let i=1; i<=5; i++){
+        let star = document.getElementById("star" + i);
+        if (i <= rating){
+            star.classList.add("filled");
+        } else {
+            star.classList.remove("filled");
+        }
+    }
+}
+
+for(let i=1; i<=5; i++){
+    let star = document.getElementById("star" + i);
+    star.addEventListener("click", function(){
+        changeStarRating(i);
+    });
+}
+
 renderMovie(movieData);
--- data.js
+++ data.js
@@ -1,9 +1,8 @@
-let movieData ={
+module.exports = { 
     //movieinfo goes here
     movieTitle:"Bad taste",
     movieParagraph: "The population of a small town disappears and is replaced by aliens that chase human flesh for their intergalactic fast-food chain.",
     moviePicture:"https://images-na.ssl-images-amazon.com/images/M/MV5BZjI5ZDY5OWYtOTIwMy00MzQ3LWFlM2EtYjMzODIzZGU0NGUxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UY268_CR4,0,182,268_AL_.jpg",
-    movieActors:["Peter JAcksson", "Someone else", "A nerd"]
-
-
+    movieActors:["Peter Jacksson", "Someone else", "A nerd"]
+    
 };

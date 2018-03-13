190f20e208469edda425beebb379d80f8718349a
William Eriksson <william.eriksson85@gmail.com>
2018-03-13 14:43:03 +0100

    oops

---
+++ data.js
@@ -0,0 +1,9 @@
+let movieData ={
+    //movieinfo goes here
+    movieTitle:"Bad taste",
+    movieParagraph: "The population of a small town disappears and is replaced by aliens that chase human flesh for their intergalactic fast-food chain.",
+    moviePicture:"https://images-na.ssl-images-amazon.com/images/M/MV5BZjI5ZDY5OWYtOTIwMy00MzQ3LWFlM2EtYjMzODIzZGU0NGUxXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UY268_CR4,0,182,268_AL_.jpg",
+    movieActors:["Peter JAcksson", "Someone else", "A nerd"]
+
+
+};
